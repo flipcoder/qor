2ef0980bd1d30743cbccc74ceb5211ae31b248cf
Grady O'Connell <flipcoder@gmail.com>
2013-12-27 13:23:08 -0800

    fixed tile wrap

--- src/Graphics.h
+++ src/Graphics.h
@@ -36,7 +36,7 @@ public:
 
     Color()
     {
-        for(auto i: c)
+        for(auto& i: c)
             i = 1.0f;
     }
     Color(const Color& b){
@@ -219,30 +219,10 @@ struct Prefab
         float min = 0.0f,
         float max = 1.0f,
         float depth = 0.0f
-    ){
-        return {
-            glm::vec3(min, min, depth),
-            glm::vec3(min, max, depth),
-            glm::vec3(max, min, depth),
-            glm::vec3(max, max, depth),
-            glm::vec3(max, min, depth),
-            glm::vec3(min, max, depth)
-        };
-    }
+    );
 
     // Wrap/UV coordinates for quad above
-    static std::vector<glm::vec2> quad_wrap()
-    {
-        return {
-            glm::vec2(0.0f, 0.0f),
-            glm::vec2(1.0f, 0.0f),
-            glm::vec2(0.0f, 1.0f),
-            glm::vec2(1.0f, 1.0f),
-            glm::vec2(0.0f, 1.0f),
-            glm::vec2(1.0f, 0.0f)
-        };
-    }
-
+    static std::vector<glm::vec2> quad_wrap();
 
     enum Flags {
         H_FLIP = kit::bit(0),
@@ -256,48 +236,7 @@ struct Prefab
         glm::uvec2 tileset_size,
         unsigned int index = 0,
         unsigned int flags = 0
-    ){
-        auto num_tiles = glm::uvec2(
-            (tileset_size.x / tile_size.x),
-            (tileset_size.y / tile_size.y)
-        );
-
-        if(index >= num_tiles.x*num_tiles.y)
-            return std::vector<glm::vec2>();
-
-        auto unit = glm::vec2(
-            1.0f / num_tiles.x,
-            1.0f / num_tiles.y
-        );
-
-        float fi = unit.x * (index % num_tiles.x);
-        float fj = unit.y * (index / num_tiles.x);
-
-        if(flags & H_FLIP)
-        {
-            return std::vector<glm::vec2>{
-                glm::vec2(fi + unit.x, fj),
-                glm::vec2(fi + unit.x, fj + unit.y),
-                glm::vec2(fi, fj),
-
-                glm::vec2(fi, fj + unit.y),
-                glm::vec2(fi, fj),
-                glm::vec2(fi + unit.x, fj + unit.y)
-            };
-        }
-        else
-        {
-            return std::vector<glm::vec2>{
-                glm::vec2(fi, fj),
-                glm::vec2(fi, fj + unit.y),
-                glm::vec2(fi + unit.x, fj),
-
-                glm::vec2(fi + unit.x, fj + unit.y),
-                glm::vec2(fi + unit.x, fj),
-                glm::vec2(fi, fj + unit.y)
-            };
-        }
-    }
+    );
 };
 
 enum class Space
@@ -308,5 +247,4 @@ enum class Space
 };
 
 #endif
-
 
