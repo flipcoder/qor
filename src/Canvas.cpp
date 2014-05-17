e26541dabe87a6cd367c5ebe84400ebb5b3579e1
Grady O'Connell <flipcoder@gmail.com>
2014-05-17 01:41:00 -0700

    canvas needs to clamp here

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -34,6 +34,8 @@ Canvas :: Canvas(unsigned w, unsigned h):
             glBindTexture(GL_TEXTURE_2D, last_id);
         };
         
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         
