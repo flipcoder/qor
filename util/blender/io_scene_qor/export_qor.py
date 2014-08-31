168b20167bc7eabedec4b0b97e84fe36fe98c9f7
Grady O'Connell <flipcoder@gmail.com>
2014-08-31 16:56:50 -0700

    more work on blender exporter

--- util/blender/io_scene_qor/export_qor.py
+++ util/blender/io_scene_qor/export_qor.py
@@ -1,5 +1,6 @@
 import json
 import bpy
+import itertools
 from mathutils import *
 
 def mesh_triangulate(mesh):
@@ -10,33 +11,72 @@ def mesh_triangulate(mesh):
     bm.to_mesh(mesh)
     bm.free()
 
+blender_matrix = Matrix((
+    (0.0, 1.0, 0.0),
+    (0.0, 0.0, 1.0),
+    (-1.0, 0.0, 0.0)
+))
+
+#def from_blender_space(m):
+#    r = m * blender_matrix
+    #r[2][0], r[0][0] = r[0][0], r[2][0]
+    #return r
+
+def vec(v):
+    v.z, v.x, v.y = -v.x, v.y, v.z
+    #return v * blender_matrix
+    return v
+
+def mat(m):
+    #v.z, v.x, v.y = -v.x, v.y, v.z
+    return m * blender_matrix
+
+def iterate_object(scene, obj, context, buf):
+    node = {}
+    
+    if obj.type == "MESH":
+        mesh = obj.to_mesh(bpy.context.scene, settings="PREVIEW", apply_modifiers=True)
+        mesh_triangulate(mesh)
+        node = {
+            'name': obj.name,
+            'type': 'mesh',
+            'matrix': list(itertools.chain(*map(lambda v: v.to_tuple(), (obj.matrix_local))))
+        }
+    elif obj.type == "LAMP":
+        node = {
+            'name': obj.name,
+            'type': 'light',
+            'matrix': list(itertools.chain(*map(lambda v: v.to_tuple(), (obj.matrix_local))))
+        }
+    elif obj.type == "CAMERA":
+        node = {
+            'name': obj.name,
+            'type': 'camera',
+            'matrix': list(itertools.chain(*map(lambda v: v.to_tuple(), (obj.matrix_local))))
+        }
+    else:
+        pass
+    
+    for ch in obj.children:
+        iterate_object(scene, ch, context, node)
+    
+    if node:
+        buf["nodes"] += [node]
+
 def save(operator, context, filepath=""):
 
     buf = {}
     buf["nodes"] = []
     
+    buf["scene"] = {}
+    buf["scene"]["gravity"] = vec(bpy.context.scene.gravity).to_tuple(4)
+    
     for base in bpy.context.scene.object_bases:
+        iterate_object(bpy.context.scene, base.object, context, buf)
         
-        node = {}
-        obj = base.object
-        
-        if obj.type == "MESH":
-            mesh = obj.to_mesh(bpy.context.scene, settings="PREVIEW", apply_modifiers=True)
-            mesh_triangulate(mesh)
-            node = {'type': 'mesh'}
-        elif obj.type == "LAMP":
-            node = {'type': 'light'}
-        elif obj.type == "CAMERA":
-            node = {'type': 'camera'}
-        else:
-            pass
-        
-        if node:
-            buf["nodes"] += [node]
-        
-        out = open(filepath, "w")
-        out.write(json.dumps(buf))
-        out.close()
+    out = open(filepath, "w")
+    out.write(json.dumps(buf, indent=4))
+    out.close()
 
     return {"FINISHED"}
 
