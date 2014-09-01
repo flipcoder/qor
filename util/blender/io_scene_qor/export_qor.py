298602f9573c9a91d861e9d3ddd5238d91813d1c
Grady O'Connell <flipcoder@gmail.com>
2014-08-31 18:33:17 -0700

    continue work on blender exporter

--- util/blender/io_scene_qor/export_qor.py
+++ util/blender/io_scene_qor/export_qor.py
@@ -11,11 +11,11 @@ def mesh_triangulate(mesh):
     bm.to_mesh(mesh)
     bm.free()
 
-blender_matrix = Matrix((
-    (0.0, 1.0, 0.0),
-    (0.0, 0.0, 1.0),
-    (-1.0, 0.0, 0.0)
-))
+#blender_matrix = Matrix((
+#    (0.0, 1.0, 0.0),
+#    (0.0, 0.0, 1.0),
+#    (-1.0, 0.0, 0.0)
+#))
 
 #def from_blender_space(m):
 #    r = m * blender_matrix
@@ -29,50 +29,77 @@ def vec(v):
 
 def mat(m):
     #v.z, v.x, v.y = -v.x, v.y, v.z
-    return m * blender_matrix
+    return m
+    #return m * blender_matrix
 
-def iterate_object(scene, obj, context, buf):
+def iterate_object(scene, obj, context, nodes):
     node = {}
     
     if obj.type == "MESH":
         mesh = obj.to_mesh(bpy.context.scene, settings="PREVIEW", apply_modifiers=True)
-        mesh_triangulate(mesh)
+        #mesh_triangulate(mesh)
         node = {
             'name': obj.name,
+            'data': obj.data.name,
             'type': 'mesh',
             'matrix': list(itertools.chain(*map(lambda v: v.to_tuple(), (obj.matrix_local))))
         }
+    elif obj.type == "SPEAKER":
+        node = {
+            'name': obj.name,
+            'data': obj.data.name,
+            'type': 'sound',
+            'volume': obj.data.volume,
+            'pitch': obj.data.pitch
+        }
+        if obj.data.sound:
+            node["sound"] = obj.data.sound.name
     elif obj.type == "LAMP":
         node = {
             'name': obj.name,
+            'data': obj.data.name,
             'type': 'light',
+            'light': obj.data.type,
+            'energy': obj.data.energy,
+            'color': list(obj.data.color),
+            'specular': obj.data.use_specular,
+            'diffuse': obj.data.use_diffuse,
+            'distance': obj.data.distance,
             'matrix': list(itertools.chain(*map(lambda v: v.to_tuple(), (obj.matrix_local))))
         }
     elif obj.type == "CAMERA":
         node = {
             'name': obj.name,
-            'type': 'camera',
+            'data': obj.data.name,
             'matrix': list(itertools.chain(*map(lambda v: v.to_tuple(), (obj.matrix_local))))
         }
     else:
         pass
     
+    if obj.keys():
+        node["properties"] = {}
+    #for k in obj.keys():
+    #    node["properties"][k] = obj.get(k)
+    
+    if obj.children:
+        node["nodes"] = []
+    
     for ch in obj.children:
-        iterate_object(scene, ch, context, node)
+        iterate_object(scene, ch, context, node["nodes"])
     
     if node:
-        buf["nodes"] += [node]
+        nodes += [node]
 
 def save(operator, context, filepath=""):
 
     buf = {}
-    buf["nodes"] = []
     
     buf["scene"] = {}
     buf["scene"]["gravity"] = vec(bpy.context.scene.gravity).to_tuple(4)
     
+    buf["scene"]["nodes"] = []
     for base in bpy.context.scene.object_bases:
-        iterate_object(bpy.context.scene, base.object, context, buf)
+        iterate_object(bpy.context.scene, base.object, context, buf["scene"]["nodes"])
         
     out = open(filepath, "w")
     out.write(json.dumps(buf, indent=4))
