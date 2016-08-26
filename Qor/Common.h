e1a3f4427921492bf62817682c836dc05858d2bf
Grady O'Connell <flipcoder@gmail.com>
2016-08-26 12:25:04 -0700

    apple GL headers

--- Qor/Common.h
+++ Qor/Common.h
@@ -2,13 +2,16 @@
 #define _COMMON_H_C7X7FP4Q
 
 #ifdef _MSC_VER
-#include <SDL2/SDL_main.h>
+    #include <SDL2/SDL_main.h>
 #endif
 
 #include <SDL2/SDL.h>
-//#include <GL3/gl3.h>
-//#include <GL3/gl3w.h>
 #include <GL/glew.h>
-#include <GL/gl.h>
+
+#ifdef __APPLE__
+    #include <OpenGL/gl.h>
+#else
+    #include <GL/gl.h>
+#endif
 
 #endif
